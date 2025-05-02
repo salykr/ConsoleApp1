98718b1a4d697094b72c5684d29ebd207c646f1e
unknown <sallykreik@gmail.com>
2025-05-02 11:23:07 +0300

    removed comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using OtpNet;                      // For TOTP key generation and validation
-using ZXing;                      // QR code generation
+using OtpNet;                      
+using ZXing;                      
 using ZXing.Common;
 using ZXing.Rendering;
-using SixLabors.ImageSharp;       // Image processing
+using SixLabors.ImageSharp;      
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Formats.Png;
-using System.Diagnostics;         // For opening the image file
+using System.Diagnostics;        
 
 namespace TwoFactorApp;
 
@@ -16,21 +16,14 @@ class Program
     {
         Console.WriteLine("Two-Factor Authentication Setup");
 
-        //generated key: class OTP
         byte[] secretBytes = KeyGeneration.GenerateRandomKey(20);
-        //encode the secret to the format of TOTP secret
 
-        //because the secret needs to be shared with the user or an authenticator app
-        //and raw bytes (byte[]) can't be easily displayed, copied, or scanned.
         String base32Secret = Base32Encoding.ToString(secretBytes);
-        //Console.WriteLine($"Secret key (save this securely): {base32Secret}");
 
-                    // otpauth://totp/{issuer}:{account}?secret={key}&issuer={issuer}
         String uri = $"otpauth://totp/MyApp:user@example.com?secret={base32Secret}&issuer=MyApp";
 
         Console.WriteLine("\nGenerating QR code image...");
         SaveQrCodeToImage(uri, "qrcode.png");
-        //Console.WriteLine("QR code saved and opened.");
 
         Totp totp = new Totp(secretBytes);
         for (int attempt = 1; attempt <= 3; attempt++)
@@ -49,7 +42,6 @@ class Program
 
     static void SaveQrCodeToImage(string content, string outputPath)
     {
-        //creating QR code
         BarcodeWriterPixelData writer = new BarcodeWriterPixelData
         {
             Format = BarcodeFormat.QR_CODE,
@@ -60,19 +52,11 @@ class Program
                 Margin = 1
             }
         };
-
-        //pixelData.Pixels: byte[] containing raw RGBA values( for each pixel).
-        //Converts the input string (content) into raw pixel data representing the QR code
-        // QR code as raw pixel data
         PixelData pixelData = writer.Write(content);
 
-        //interpret each set of 4 bytes as an RGBA pixel, and build an image of size
-        //Create an image from raw pixels
         using Image image = Image.LoadPixelData<Rgba32>(pixelData.Pixels, pixelData.Width, pixelData.Height);
 
         image.Save(outputPath, new PngEncoder());
-
-        //open the image automatically
         try
         {
             Process.Start(new ProcessStartInfo
